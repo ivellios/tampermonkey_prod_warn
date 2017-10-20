1e056229d42e4f321cabbda91bc0861f162a18df
Janusz Kamienski <janusz.kamienski@deployed.pl>
2017-10-20 15:54:16 +0200

    added better bars hiding and showing back after 5 seconds

--- Production_Warning_Tape.user.js
+++ Production_Warning_Tape.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
 // @name         Production Warning Tape
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Displays vertical bars on given websites. Usefull when you use to visit production site and don't want to mistake it for local dev.
-// @author       Janusz "Ivellios" Kamienski
+// @version      1.1
+// @description  Displays vertical bars on given websites. Usefull when you use to visit production site and don't want to mistake it for local dev. Bars hide when hovered, but return after 5 seconds.
+// @author       Janusz Kamienski
 // @grant        none
 // ==/UserScript==
 
-CSS = `.maintenance-information {top:0;bottom:0;position:fixed;height:100vh;width:50px;z-index:99999;background-image: repeating-linear-gradient(145deg, rgba(255,255,255,0.8), rgba(255,255,255,0.8) 30px, rgba(249,112,114,0.8) 30px, rgba(249,112,114,0.8) 60px);z-index:9999;}
+CSS = `.maintenance-information {top:0;bottom:0;position:fixed;height:100vh;width:70px;z-index:99999;background-image: repeating-linear-gradient(145deg, rgba(255,255,255,0.8), rgba(255,255,255,0.8) 30px, rgba(249,112,114,0.8) 30px, rgba(249,112,114,0.8) 60px);z-index:9999;}
 .maintenance-information.left{left:0;}
 .maintenance-information.right{right:0;}
-.maintenance-information p{writing-mode: tb-rl;padding:10px 0;margin: auto;color:#333;font-weight:bold;font-size:24px;text-align:center;text-shadow:0 0 15px white;}
-.maintenance-information:hover{display:none;}
+.maintenance-information p{writing-mode: tb-rl;padding:20px 0;margin: auto;color:#333;font-weight:bold;font-size:24px;text-align:center;text-shadow:0 0 15px white;}
 `;
 
 TEXT = "PRODUCTION!";
@@ -28,6 +27,23 @@ TEXT = "PRODUCTION!";
         document.body.appendChild(div);
     }
 
+    function addListeners(){
+        var bars = document.getElementsByClassName('maintenance-information');
+
+        for(var i=0; i < bars.length; i++){
+            bars[i].addEventListener('mouseover', () => {
+                for(var j=0; j < bars.length; j++){
+                    bars[j].style.display = "none";
+                }
+                setTimeout(() => {
+                    for(var j=0; j < bars.length; j++){
+                        bars[j].style.display = "block";
+                    }
+                }, 5000);
+            }, false);
+        }
+    }
+
     var style = document.createElement('style');
     if (style.styleSheet) {
         style.styleSheet.cssText = CSS;
@@ -38,4 +54,5 @@ TEXT = "PRODUCTION!";
 
     createBar("left");
     createBar("right");
+    addListeners();
 })();
